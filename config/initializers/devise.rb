98aff8dead198d0935557278b0ad4bbe23c334ad
Steven Torrence <storrence88@gmail.com>
2021-08-12 22:32:57 -0500

    Add flash messages and fix rubocop errors

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class TurboFailureApp < Devise::FailureApp
   def respond
     if request_format == :turbo_stream
@@ -9,7 +10,7 @@ class TurboFailureApp < Devise::FailureApp
   end
 
   def skip_format?
-    %w(html turbo_stream */*).include? request_format.to_s
+    %w[html turbo_stream */*].include? request_format.to_s
   end
 end
 
