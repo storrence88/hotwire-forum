3452399107daf96c4daebbf829740a30ff0b5264
Steven Torrence <storrence88@gmail.com>
2021-08-13 23:52:34 -0500

    Setting the current user for all requests across the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_current_user, if: :user_signed_in?
 
   private
+
+  def set_current_user
+    Current.user = current_user
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
