951a05e795f515081ab37f2fcef7cfa36ded01ee
Steven Torrence <storrence88@gmail.com>
2021-08-15 21:16:19 -0500

    Editing disucssions

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -2,6 +2,7 @@
 
 class DiscussionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_discussion, only: %i[edit update]
 
   def index
     @discussions = Discussion.all
@@ -23,7 +24,24 @@ class DiscussionsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @discussion.update(discussion_params)
+        format.html { redirect_to discussions_path, notice: 'Discussion was successfully updated.' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+
+  def set_discussion
+    @discussion = Discussion.find(params[:id])
+  end
 
   def discussion_params
     params.require(:discussion).permit(:name, :closed, :pinned)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  resources :discussions, only: %i[index new create]
+  resources :discussions
   root 'main#index'
 end
