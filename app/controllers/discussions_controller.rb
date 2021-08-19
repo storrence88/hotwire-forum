bceef9d5b383773803a578c67a1c5b5e42b08fd3
Steven Torrence <storrence88@gmail.com>
2021-08-18 22:58:48 -0500

    Build header partial and add turbo frame tags for inline editing

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -2,7 +2,7 @@
 
 class DiscussionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_discussion, only: %i[edit update destroy]
+  before_action :set_discussion, only: %i[show edit update destroy]
 
   def index
     @discussions = Discussion.all
@@ -12,12 +12,15 @@ class DiscussionsController < ApplicationController
     @discussion = Discussion.new
   end
 
+  def show
+  end
+
   def create
     @discussion = Discussion.new(discussion_params)
 
     respond_to do |format|
       if @discussion.save
-        format.html { redirect_to discussions_path, notice: 'Discussion was successfully created.' }
+        format.html { redirect_to @discussion, notice: 'Discussion was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -30,7 +33,7 @@ class DiscussionsController < ApplicationController
   def update
     respond_to do |format|
       if @discussion.update(discussion_params)
-        format.html { redirect_to discussions_path, notice: 'Discussion was successfully updated.' }
+        format.html { redirect_to @discussion, notice: 'Discussion was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
