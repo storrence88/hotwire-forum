0ac96637d8ccd3a95cfe185bd058d2538cdf4f58
Steven Torrence <storrence88@gmail.com>
2021-08-23 19:57:08 -0500

    Making realtime posts on the discussion show page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   belongs_to :discussion, counter_cache: true, touch: true
   belongs_to :user, default: -> { Current.user }
-  
+
   has_rich_text :body
 
   validates :body, presence: true
+
+  after_create_commit -> { broadcast_append_to discussion, partial: 'discussions/posts/post', locals: { post: self } }
 end
