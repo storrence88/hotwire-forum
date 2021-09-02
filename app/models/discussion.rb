bdcc34882b6b782b2beb7145fc3ac508aebf9ce5
Steven Torrence <storrence88@gmail.com>
2021-09-01 20:23:17 -0500

    Add categories to discussions index

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
   belongs_to :category, counter_cache: true, touch: true
+
+  delegate :name, prefix: true, to: :category
 
   has_many :posts, dependent: :destroy
 
